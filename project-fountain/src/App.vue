2e91eb946a3a416d2f59657dc979ba9375817058
tijlclottemans <tijlclottemans@gmail.com>
2020-10-31 17:16:02 +0100

    created-project-imported-fountain

---
+++ project-fountain/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <fountain-scene />
+  </div>
+</template>
+
+<script>
+import fountainScene from './components/scene.vue'
+
+export default {
+  name: 'App',
+  components: {
+    fountainScene 
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ project-fountain/src/components/scene.vue
@@ -0,0 +1,20 @@
+<template>
+<Scene>
+  <Asset :src="path" :scaling="[0.5, 0.5, 0.5]" :position="[-1, -7.25, -0.5]" :rotation="[0, Math.PI, 0]"></Asset>
+</Scene>
+</template>
+<script>
+import '@babylonjs/loaders';
+import { Scene, Asset } from 'vue-babylonjs';
+import Fountain from "file-loader!../../public/models/fountain.babylon"
+export default {
+  name: 'fountain-scene',
+  components: { Scene, Asset },
+  data: () => ({
+    path: Fountain
+  })
+}
+</script>
+
+<style scoped>
+</style>
